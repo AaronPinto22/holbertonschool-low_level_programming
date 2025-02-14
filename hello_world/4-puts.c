3b02933052d88f2106fd4343cdf5260daf79c248
AaronPinto22 <aaronpinto2210@hotmail.com>
2025-02-14 12:52:01 +0000

    betty stop

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-/* main returns (0) */
-
 int main(void)
 {
 
-/* puts prints out a message followed by a new line */
+/** puts prints out a message followed by a new line */
 
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
