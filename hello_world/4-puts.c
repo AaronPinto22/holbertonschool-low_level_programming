88d651947a3ee5fb62b7c6fce5afa2f2fcb427de
AaronPinto22 <aaronpinto2210@hotmail.com>
2025-02-14 12:44:17 +0000

    no n

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
-/* function puts prints out message */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
