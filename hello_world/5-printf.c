539fcbb430479b1611ad2b1c1d428fc3d9ac43c9
AaronPinto22 <aaronpinto2210@hotmail.com>
2025-02-14 14:11:32 +0000

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main printf uses directory command to print line
+ * return value is 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	return (0);
