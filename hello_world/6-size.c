eb76f9638208a24701d1f9e1e3c1386b226080c8
AaronPinto22 <aaronpinto2210@hotmail.com>
2025-02-14 16:07:47 +0000

    sizes

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 
 int main(void)
-{ 
-    	printf("char size: %lu bytes\n", (unsigned long)sizeof(char));
-	printf("int size: %lu bytes\n", (unsigned long)sizeof(int));
-	printf("long int size: %lu bytes\n", (unsigned long)sizeof(long int));
-    	printf("long long int size: %lu bytes\n", (unsigned long)sizeof(long long int));
-    	printf("float size: %lu bytes\n", (unsigned long)sizeof(float));
-    	return (0);
+{
+    printf("char size: %lu bytes\n", (unsigned long)sizeof(char));
+    printf("int size: %lu bytes\n", (unsigned long)sizeof(int));
+    printf("long int size: %lu bytes\n", (unsigned long)sizeof(long int));
+    printf("long long int size: %lu bytes\n", (unsigned long)sizeof(long long int));
+    printf("float size: %lu bytes\n", (unsigned long)sizeof(float));
+    return (0);
 }
