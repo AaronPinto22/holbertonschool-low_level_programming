f410164d22b294070b12b798caf29a7ac2caeb12
AaronPinto22 <aaronpinto2210@hotmail.com>
2025-02-14 16:33:27 +0000

    sizealgo

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-    printf("char size:%zu byte(s)\n", sizeof(char));
-    printf("int size:%zu bytes\n", sizeof(int));
-    printf("long int size:%zu byte(s)\n", sizeof(long));
-    printf("long long int size:%zu byte(s)\n", sizeof(long long int));
-    printf("float size:%zu byte(s)\n", sizeof(float));
+    printf("char size: %zu byte(s)\n", sizeof(char));
+    printf("int size: %zu bytes\n", sizeof(int));
+    printf("long int size: %zu byte(s)\n", sizeof(long int));
+    printf("long long int size: %zu byte(s)\n", sizeof(long long int));
+    printf("float size: %zu byte(s)\n", sizeof(float));
     return (0);
 }
