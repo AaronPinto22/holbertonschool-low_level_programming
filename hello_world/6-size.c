2f8bf606a8ac2a997aceb97a1a7f1bb4a5ff6a73
AaronPinto22 <aaronpinto2210@hotmail.com>
2025-02-14 15:52:12 +0000

    again we try

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,9 +6,14 @@
  */
 
 int main(void)
-{
-	printf("char size: %zu \n", sizeof(char));
-	printf("int size: %zu \n", sizeof(int));
-	printf("float size: %zu \n", sizeof(float));
-       return (0);
+{ 
+    printf("Size of char: %zu bytes\n", sizeof(char));
+    printf("Size of short: %zu bytes\n", sizeof(short));
+    printf("Size of int: %zu bytes\n", sizeof(int));
+    printf("Size of long: %zu bytes\n", sizeof(long));
+    printf("Size of long long: %zu bytes\n", sizeof(long long));
+    printf("Size of float: %zu bytes\n", sizeof(float));
+    printf("Size of double: %zu bytes\n", sizeof(double));
+    printf("Size of long double: %zu bytes\n", sizeof(long double));    printf("Size of void pointer: %zu bytes\n", sizeof(void*));
+    return (0);
 }
