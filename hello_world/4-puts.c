e03a3edde7b9ef72124fb6be47f23af7430325a8
AaronPinto22 <aaronpinto2210@hotmail.com>
2025-02-14 13:46:40 +0000

    betty pleaseagain

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,12 @@
-/**
- * puts prints out a line with a new line at the end
- */
-
 #include <stdio.h>
+
+/**
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-/**
- * main returns 0
- */
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
