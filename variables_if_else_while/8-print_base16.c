68da39cdd7ac58bcc4c955c01f706ca401db5f4b
root <aaronpinto2210@hotmail.com>
2025-02-17 17:12:15 +0000

    hexa

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - prints alphabet
+ *Return: 0
+ */
+
+int main(void)
+{
+int i;
+char hex_digits[] = "0123456789abcdef";
+for (i = 0; i <= 16; i++)
+{
+putchar(hex_digits[i]);
+}
+putchar ('\n');
+return (0);
+}
