1a5ccbdf1b1377d6bc6feba9c863c7430f544966
root <aaronpinto2210@hotmail.com>
2025-02-20 13:29:37 +0000

    print10x

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * void print_alphabet_x10(void) - print 10x
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+char i;
+_putchar(i <= 'z' ? i : '\n');
+}
+_putchar ('\n');
+}
