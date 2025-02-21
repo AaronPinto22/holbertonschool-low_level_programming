dc43d229335890c63563a997b8f80fefa3872ad1
root <aaronpinto2210@hotmail.com>
2025-02-21 17:58:47 +0000

    pause

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prinst last digit
+ * Return: 1
+ * @n: int
+ */
+
+int print_last_digit(int);
+{
+	int = n %10;
+
+    	_putchar(n); 
+    
+    	return (0);
+}
