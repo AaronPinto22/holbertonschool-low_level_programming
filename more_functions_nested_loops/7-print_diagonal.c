3973d32d6148ea13883b03f5913c07aec63e258d
root <aaronpinto2210@hotmail.com>
2025-02-24 17:40:55 +0000

    feat: diagonal test

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line
+ * Return: 0
+ * @n: int
+ */
+
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+			for (j = 0; j < i; j++)
+			{
+			_putchar(' ');
+			}
+		_putchar('\\');
+	}
+	_putchar('\n');
+}
