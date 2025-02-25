3679c224e3960347c3693ed2a00d28be7fed78be
root <aaronpinto2210@hotmail.com>
2025-02-25 15:35:29 +0000

    fix: indentation

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -15,11 +15,15 @@ void print_square(int size)
 	for (a = 0; a < size; a++)
 	{
 	for (b = 0; b < size; b++)
-	
-		_putchar('#');
 	{
+		_putchar('#');
+	}
 		_putchar('\n');
 	}
 	}
+	else
+	{
+		_putchar('\n');
 	}
+
 }
